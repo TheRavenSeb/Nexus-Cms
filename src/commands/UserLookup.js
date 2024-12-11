bbfa165923a7957aee69e4a43bb3a3def2c2bc9a
TheRavenSeb <cdawgthomas@gmail.com>
2024-12-11 10:15:09 -0500

    added user lookup

---
+++ src/commands/UserLookup.js
@@ -0,0 +1,56 @@
+const {EmbedBuilder, SlashCommandBuilder} = require('discord.js');
+const User = require('../schemas/User');
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('info')
+    .setDescription('Chek your account information')
+    .addSubCommand(subCommand => subCommand.setName("user").setDescription("Lookup a user's information").addUserOption(option => option.setName("user").setDescription("The user to lookup"))),
+  async execute(interaction) {
+    if(interaction.options.getSubcommand() === 'user'){
+        const user = interaction.options.getUser('user');
+    var role = ''
+
+    User.findOne({ DiscordId: interaction.user.id }).then(user => {
+        if (!user) {
+            return interaction.reply({ content: 'You are not in the database!'});
+        }
+        if(user.IsInstructor){
+            role += 'Instructor\n'
+        }
+        if(user.IsEnlisted){
+            role += 'Enlisted\n'
+        }
+        if(user.IsOfficer){
+            role += 'Officer\n'
+        }
+        if(user.IsNCO){
+            role += 'NCO\n'
+        }
+        if(user.IsRecruit){
+            role += 'Recruit\n'
+        }
+        if(user.IsVeteran){
+            role += 'Veteran\n'
+        }
+        
+
+        const embed = new EmbedBuilder()
+            .setTitle(`Account Information for ${user.Name}`)
+            .setDescription('Here is your account information')
+            .setColor('#0099ff')
+            .setTimestamp()
+            .setFields(
+                { name: 'Rank', value: user.Rank, inline: true },
+                { name: 'MOS', value: user.Mos, inline: true },
+                { name: 'Join Date', value: user.JoinDate.toDateString(), inline: true },
+                { name: 'Combat Hours', value: user.CombatHours.toString(), inline: true },
+                { name: 'Combat Minutes', value: user.CombatMinutes.toString(), inline: true },
+                { name: 'Roles', value: role, inline: true }
+            )
+            
+        interaction.reply({ embeds: [embed] });
+    })
+}
+
+  }
+}
