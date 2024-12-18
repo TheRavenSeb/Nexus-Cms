18fa18f3a4bf96c9c99c5b116765ba04ec4cbc55
TSBsebseb <cdawgthomas@gmail.com>
2024-12-17 19:25:23 -0500

    refactor: remove unnecessary trailing commas in UserLookup.js

--- src/commands/UserLookup.js
+++ src/commands/UserLookup.js
@@ -52,9 +52,9 @@ module.exports = {
                 { name: 'Rank', value: user.Rank, inline: true },
                 { name: 'MOS', value: user.Mos, inline: true },
                 { name: 'Join Date', value: user.JoinDate.toDateString(),  },
-                { name: 'Combat Hours', value: user.CombatHours.toString(),  },
+                { name: 'Combat Hours', value: user.CombatHours.toString()  },
                 { name: 'Combat Minutes', value: user.CombatMinutes.toString(), inline: true },
-                { name: 'Good Combat Hours', value: user.GoodCombatHours.toString(),  },
+                { name: 'Good Combat Hours', value: user.GoodCombatHours.toString()  },
                 { name: 'Good Combat Minutes', value: user.GoodCombatMinutes.toString(), inline: true },
                 { name: 'Roles', value: role}
             )
